65abacc9dff86630317d4ac909fd0781f46543ec
lumozx <969671691@qq.com>
2021-04-23 14:16:08 +0800

    feat: 倒计时

--- src/pages/random/index.js
+++ src/pages/random/index.js
@@ -8,12 +8,19 @@ import SEO from "../../components/seo"
 import tiger from "../../../static/tiger"
 import * as randomStyle from "./random.module.css"
 
+
+const baseTime = 5
+
 class Random extends React.Component {
   constructor(props) {
     super(props)
     this.setPerson = this.setPerson.bind(this)
     this.state = {
       person: [],
+      time: baseTime,
+      res: {
+        data: 1,
+      },
     }
   }
   setPerson() {
@@ -22,26 +29,47 @@ class Random extends React.Component {
       this.setState(
         {
           person: person.split(","),
+          time: baseTime,
         },
         () => {
           if (document.getElementById("toggle")) {
             const lottery = new tiger(
               document.getElementById("toggle"),
-              document.querySelectorAll(".list")
+              document.querySelectorAll(".list"),
+              {
+                aniMinTime:baseTime * 1000
+              }
             )
-            document.getElementById("toggle").addEventListener("click", () => {
-              lottery.draw()
-            })
-            lottery.on("start", async () => {
-              if (this.state.person.length === 1) {
-                lottery.setResult([0])
-              } else {
+            document
+              .getElementById("toggle")
+              .addEventListener("click", async () => {
                 const res = await axios.get(
                   `https://www.random.org/integers/?num=1&min=1&max=${this.state.person.length}&col=1&base=10&format=plain&rnd=new`
                 )
+                this.setState(
+                  {
+                    res,
+                  },
+                  () => lottery.draw()
+                )
+              })
+            lottery.on("start", async () => {
+              this.setState({
+                time: baseTime,
+              })
+              const time = setInterval(() => {
+                this.setState({
+                  time: this.state.time - 1,
+                })
+                if (this.state.time <= 0) clearInterval(time)
+              }, 1000)
+
+              if (this.state.person.length === 1) {
+                lottery.setResult([0])
+              } else {
                 setTimeout(() => {
-                  lottery.setResult([Number(res.data) - 1])
-                }, 500)
+                  lottery.setResult([Number(this.state.res.data) - 1])
+                }, 500);
               }
             })
           }
@@ -51,6 +79,7 @@ class Random extends React.Component {
   }
   render() {
     let tigerCom = null
+    let time = null
     if (this.state.person.length > 0) {
       const item = this.state.person.map(item => (
         <div className={randomStyle.item} key={`${item}${Math.random()}`}>
@@ -67,6 +96,7 @@ class Random extends React.Component {
           </a>
         </div>
       )
+      time = <div className={randomStyle.time}>{this.state.time}</div>
     }
     return (
       <StaticQuery
@@ -82,6 +112,7 @@ class Random extends React.Component {
               <div className={randomStyle.person}>
                 {this.state.person.join(",")}
               </div>
+              {time}
               {tigerCom}
             </Layout>
           )
