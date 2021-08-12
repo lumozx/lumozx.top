391e8a342fb158c39829fd515bf4fc042d0fa1c2
lumozx <969671691@qq.com>
2021-08-12 14:52:15 +0800

    feat: 增加一句话

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,18 @@ import { Link, graphql } from "gatsby"
 
 import SEO from "../components/seo"
 
+import say from '.././../static/say'
+
 const BlogIndex = ({ data }) => {
-  console.log(data)
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const name = data.site.siteMetadata?.author?.name || ""
-  const summary = data.site.siteMetadata?.author?.summary || ""
+
+  let index = Math.floor((Math.random()*say.length))
   return (
     <div className="index">
       <SEO title={siteTitle} />
       <h1 className="index__title">{name}</h1>
-      <h2 className="index__summary">{summary}</h2>
+      <h2 className="index__summary">{say[index]}</h2>
       <div className="index__link">
         <Link className="index__link-item" to="/list/">
           博客
